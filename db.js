f994632b9f3c7edc34fe1e94907a9dc6869cadcf
Harry Dearing <harrysdearing@gmail.com>
2021-03-06 12:25:10 -0500

    Check

--- db.js
+++ db.js
@@ -3,10 +3,11 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-        ssl:{
-                require: true,
-                rejectUnauthorized: false
-            }
+        // ssl:{
+        //         require: true,
+        //         rejectUnauthorized: false
+        //     }
+        ssl:true
         }
 })
 
